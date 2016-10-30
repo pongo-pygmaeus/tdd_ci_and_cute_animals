d4cd8826fbeaecbf3f040036174784dde4068979
pongo-pygmaeus <maxwellmathews@gmail.com>
2016-10-30 15:11:41 -0700

    Committing prior to Travis CI deployment

--- db/migrate/20161019175840_create_leagues.rb
+++ db/migrate/20161019175840_create_leagues.rb
@@ -1,7 +1,7 @@
 class CreateLeagues < ActiveRecord::Migration[5.0]
   def change
     create_table :leagues do |t|
-      t.string :name
+      t.string  :name
       t.integer :creator_id
 
       t.timestamps
